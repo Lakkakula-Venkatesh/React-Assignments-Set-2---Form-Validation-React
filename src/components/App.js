11f990ac695b173fd6c65a38ee02351df44d339c
Lakkakula Venkatesh <lakkakula.venkatesh8726@gmail.com>
2020-12-03 18:54:36 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,49 +2,39 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
-  const [greetMessage, setGreetMessage] = React.useState("");
   const [name, setName] = React.useState("");
   const [mail, setMail] = React.useState("");
   const [gender, setGender] = React.useState("male");
   const [number, setNumber] = React.useState("");
   const [password, setPassword] = React.useState("");
-  const [allError, setAllError] = React.useState("");
-  const [nameError, setNameError] = React.useState("");
-  const [mailError, setMailError] = React.useState("");
-  const [genderError, setGenderError] = React.useState("");
-  const [numberError, setNumberError] = React.useState("");
-  const [passwordError, setPasswordError] = React.useState("");
-
+  const [message, setMessage] = useState("");
+  
   const handleSubmit = () => {
     if(name === "" || mail === "" || gender === "" || number === "" || password === "") {
-      setGreetMessage("");
-      setAllError("All fields are mandatory");
+      setMessage("All fields are mandatory");
       return;
     }
-    setAllError("");
-    
+    if(!(/[a-z][0-9]/i.test(name))) {
+      setMessage("Name is not alphanumeric");
+      return;
+    }
     if(!mail.includes("@")) {
-      setMailError("Email must contain @");
+      setMessage("Email must contain @");
       return;
     }
-    setMailError("");
     if(isNaN(number)) {
-      setNumberError("Phone Number must contain only numbers");
+      setMessage("Phone Number must contain only numbers");
       return;
     }
-    setNumberError("");
     if(gender.toLowerCase() !== "male" && gender.toLowerCase() !== "female" && gender.toLowerCase() !== "others") {
-      setGenderError("Please identify as male, female or others");
+      setMessage("Please identify as male, female or others");
       return;
-    }
-    setGenderError("");
-    
+    }    
     if(password.length < 6) {
-      setPasswordError("Password must contain atleast 6 letters");
+      setMessage("Password must contain atleast 6 letters");
       return;
     }
-    setPasswordError("");
-    setGreetMessage(getName());
+    setMessage(getName());
   }
   const getName = () => {
     return "Hello " +  mail.split("@")[0];
@@ -67,19 +57,13 @@ const App = () => {
   return (
     <>
       <div id="main">
-        <input data-testid = 'name' onChange={handleNameChange} required value={name}/>
-        <input data-testid = 'email' onChange={handleMailChange} required value={mail} />
-        <input data-testid = 'gender' onChange={handleGenderChange} required value={gender} />
-        <input data-testid = 'phoneNumber' onChange={handleNumberChange} value={number} />
-        <input data-testid = 'password' type='password' onChange={handlePasswordChange} value={password} />
+        <input data-testid = 'name' onChange={handleNameChange} required value={name}/><br />
+        <input data-testid = 'email' onChange={handleMailChange} required value={mail} /><br />
+        <input data-testid = 'gender' onChange={handleGenderChange} required value={gender} /><br />
+        <input data-testid = 'phoneNumber' onChange={handleNumberChange} value={number} /><br />
+        <input data-testid = 'password' type='password' onChange={handlePasswordChange} value={password} /><br />
         <input data-testid = 'submit' type="submit" onClick={handleSubmit} />
-        {greetMessage !== "" && <div>{greetMessage}</div>}
-        {allError !== "" && <div>{allError}</div>}
-        {nameError !== "" && <div>{nameError}</div>}
-        {mailError !== "" && <div>{mailError}</div>}
-        {genderError !== "" && <div>{genderError}</div>}
-        {numberError !== "" && <div>{numberError}</div>}
-        {passwordError !== "" && <div>{passwordError}</div>}
+        <div>{message}</div>
       </div>
     </>
   );
